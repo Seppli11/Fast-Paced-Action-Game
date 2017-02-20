cad34a3e1c3b11641d7e25b7a1d34135e554eb92
Sebastian-Lapto\Sebastian <sebastian.zumbrunn@gmail.com>
2017-02-20 18:41:15 +0100

    Add Prototyp 1 (Movement, Teleporting)

---
+++ Project Files/Prototyp 1/Assets/Scripts/Controller.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    private InputType _currentInput;
+    public InputType currentInput
+    {
+        get { return _currentInput; }
+    }
+
+    private float _horizontalAxis = 0f;
+    public float horzontalAxis {
+        get { return _horizontalAxis; } }
+
+    private float _verticalAxis = 0f;
+    public float verticalAxis
+    {
+        get { return _verticalAxis; }
+    }
+
+    private bool _shootButton1;
+    public bool shootButton1{
+        get { return _shootButton1; }
+    }
+
+    private bool _shootButton2;
+    public bool shootButton2
+    {
+        get { return _shootButton2; }
+    }
+
+    private bool _teleportButton;
+    public bool teleportButton
+    {
+        get { return _teleportButton; }
+    }
+
+    private bool _inventoryButton;
+    public bool inventoryButton
+    {
+        get { return _inventoryButton; }
+    }
+
+    private bool _menuButton;
+    public bool menuButton
+    {
+        get { return _menuButton; }
+    }
+	// Use this for initialization
+	void Start () {
+        _currentInput = InputType.KEYBOARD;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckInputType();
+        switch(currentInput)
+        {
+            case InputType.KEYBOARD:
+                //if(Input.GetButton)
+                break;
+            case InputType.CONTROLLER:
+                break;
+            case InputType.TOUCHE:
+                break;
+        }
+	}
+
+    private void CheckInputType()
+    {
+        //check if mouse or keyboard is the current input method
+        if (isKeyboard()) _currentInput = InputType.KEYBOARD;
+    }
+
+    private bool isKeyboard()
+    {
+        return true;
+    }
+
+    float GetHorizontalAxis()
+    {
+        return 0;
+    }
+
+    float GetVerticalAxis()
+    {
+        return 0;
+    }
+
+    public enum InputType
+    {
+        KEYBOARD,
+        CONTROLLER,
+        TOUCHE
+    }
+}
---
+++ Project Files/Prototyp 1/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour {
+	private Transform transform;
+    private Rigidbody2D rigidbody;
+
+    private LastDirection lastDirection = LastDirection.UP;
+
+    public float speed;
+    public float teleportRange;
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.position = new Vector3((float)(transform.position.x + (speed * Time.deltaTime)), transform.position.y);
+            lastDirection = LastDirection.RIGHT;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.position = new Vector3((float)(transform.position.x - (speed * Time.deltaTime)), transform.position.y);
+            lastDirection = LastDirection.LEFT;
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.position = new Vector3(transform.position.x, (float)(transform.position.y + (speed * Time.deltaTime)));
+            lastDirection = LastDirection.DOWN;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.position = new Vector3(transform.position.x, (float)(transform.position.y - (speed * Time.deltaTime)));
+            lastDirection = LastDirection.UP;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            switch(lastDirection)
+            {
+                case LastDirection.UP:
+                    transform.position = new Vector3(transform.position.x, transform.position.y - teleportRange);
+                    break;
+                case LastDirection.DOWN:
+                    transform.position = new Vector3(transform.position.x, transform.position.y + teleportRange);
+                    break;
+                case LastDirection.LEFT:
+                    transform.position = new Vector3(transform.position.x - teleportRange, transform.position.y);
+                    break;
+                case LastDirection.RIGHT:
+                    transform.position = new Vector3(transform.position.x + teleportRange, transform.position.y);
+                    break; 
+            }
+        }
+    }
+
+    private enum LastDirection
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+}
